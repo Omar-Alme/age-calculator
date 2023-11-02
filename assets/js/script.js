e213d4e1494de963ea14612080882c1ef902f016
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-11-02 21:16:55 +0100

    Add id for outputs

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Get month year and day from id - INPUTS
+
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const day = document.getElementById("day");
+
+// id - OUTPUTS
+
+const monthOutput = document.getElementById("monthOutput");
+const yearOutput = document.getElementById("yearOutput");
+const dayOutput = document.getElementById("dayOutput");
